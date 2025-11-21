05fd6bd01f35d79e9272b6ecc33415b5137f9c47
Hristian233 <yanevhr@yahoo.com>
2025-11-21 15:13:43 +0200

    add a get all request

--- animal-map-backend/app/routers/markers.py
+++ animal-map-backend/app/routers/markers.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app import crud, schemas
+from app import crud, schemas, models
+from sqlalchemy import func
+from geoalchemy2 import Geometry
+from sqlalchemy import cast
 
 router = APIRouter(prefix="/markers", tags=["markers"])
 
@@ -28,3 +31,28 @@ def get_markers(
 ):
     markers = crud.get_markers_in_bounds(db, min_lat, max_lat, min_lng, max_lng)
     return markers
+
+@router.get("/all", response_model=list[schemas.Marker])
+def get_all_markers(db: Session = Depends(get_db)):
+    rows = db.query(
+        models.Marker.id,
+        models.Marker.animal,
+        models.Marker.note,
+        func.ST_Y(cast(models.Marker.location, Geometry)).label("lat"),
+        func.ST_X(cast(models.Marker.location, Geometry)).label("lng"), 
+        models.Marker.image_url,
+        models.Marker.user_id
+    ).all()
+
+    return [
+        {
+            "id": r.id,
+            "animal": r.animal,
+            "note": r.note,
+            "lat": r.lat,
+            "lng": r.lng,
+            "image_url": r.image_url,
+            "user_id": r.user_id
+        }
+        for r in rows
+    ]
