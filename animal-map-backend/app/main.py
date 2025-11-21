eb22b20d24246224fe4c936f7328d710655917da
Hristian233 <yanevhr@yahoo.com>
2025-11-21 16:51:21 +0200

    Add CORS

--- animal-map-backend/app/main.py
+++ animal-map-backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.routers import markers
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],   # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(markers.router)
 
