7e22e76f5a6e282947a630704383e082b087a1ba
Hristian233 <yanevhr@yahoo.com>
2025-11-23 12:15:50 +0200

    create list for CORS origins

--- animal-map-backend/app/main.py
+++ animal-map-backend/app/main.py
@@ -7,9 +7,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],   # React dev server
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
